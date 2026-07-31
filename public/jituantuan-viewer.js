dd92b142244291dcbdf93b6fa007b8494f2d736b
Anonymousers1087 <orangersir@gmail.com>
2026-07-31 21:48:51 +0800

    Delete public/jituantuan-viewer.js

--- public/jituantuan-viewer.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-export default function JituantuanViewerRedirect() {
-  useEffect(() => {
-    window.location.replace('/jituantuan.html')
-  }, [])
-
-  return null
-}
