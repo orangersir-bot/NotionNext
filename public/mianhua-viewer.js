6b555c2bd0e98bb35028ff0ba677b62119704b76
Anonymousers1087 <orangersir@gmail.com>
2026-08-04 20:54:36 +0800

    Delete public/mianhua-viewer.js

--- public/mianhua-viewer.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-export default function MianhuaViewerRedirect() {
-  useEffect(() => {
-    window.location.replace('/mianhua.html')
-  }, [])
-
-  return null
-}
